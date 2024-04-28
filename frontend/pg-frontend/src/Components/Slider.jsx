615a1d0c3d427d8d7bd73f0dd72ffb4b604c488d
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-04-28 09:59:03 +0530

    Create Hero section

---
+++ frontend/pg-frontend/src/Components/Slider.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/Slide.scss"
+
+const Slider = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Home! Anywhere you roam <br /> Stay in the moment. Make your
+        memories
+      </h1>
+    </div>
+  );
+};
+
+export default Slider;
