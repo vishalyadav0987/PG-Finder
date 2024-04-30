34619e5c618161fa35c0f4e1d7a4ddf707de7c57
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-04-30 06:33:04 +0530

    Create Loader Component

---
+++ frontend/pg-frontend/src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Loader';
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <div className="loader-inner"></div>
+    </div>
+  )
+}
+
+export default Loader
