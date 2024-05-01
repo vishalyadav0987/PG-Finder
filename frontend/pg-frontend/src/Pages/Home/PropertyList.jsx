784aacdb34825ae1972f43a75fe2425fab95c339
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-01 07:38:03 +0530

    Property page

---
+++ frontend/pg-frontend/src/Pages/Home/PropertyList.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import "../../Styles/TripList.scss";
+import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../../Components/Navbar";
+import ListingCard from "../../Components/ListingCard";
+import { setPropertyList } from "../../redux/state";
+// import Footer from "../components/Footer"
+import Loader from '../../Components/Loader'
+
+const PropertyList = () => {
+
+  const [loading, setLoading] = useState(true)
+  const user = useSelector((state) => state.user)
+  const propertyList = user?.propertyList;
+  console.log(user)
+
+  const dispatch = useDispatch()
+  const getPropertyList = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/v1/users/${user._id}/properties`, {
+        method: "GET"
+      })
+      const data = await response.json()
+      console.log(data)
+      dispatch(setPropertyList(data))
+      setLoading(false)
+    } catch (err) {
+      console.log("Fetch all properties failed", err.message)
+    }
+  }
+
+  useEffect(() => {
+    getPropertyList()
+  }, [])
+
+
+  return loading ?<Loader/>: (
+    <>
+      <Navbar />
+      <h1 className="title-list">Your Property List</h1>
+      <div className="list">
+        {propertyList?.map(
+          ({
+            _id,
+            creator,
+            listingPhotoPaths,
+            city,
+            province,
+            country,
+            category,
+            type,
+            price,
+            booking = false,
+          }) => (
+            <ListingCard
+              listingId={_id}
+              creator={creator}
+              listingPhotoPaths={listingPhotoPaths}
+              city={city}
+              province={province}
+              country={country}
+              category={category}
+              type={type}
+              price={price}
+              booking={booking}
+            />
+          )
+        )}
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default PropertyList;
